2fdc3ecfaa089c23dfa882bd60f289011ee04cc9
SindubsMini <117236256+SindubsMini@users.noreply.github.com>
2022-11-06 18:49:28 +0300

    fixed credits

--- Doors/Script.lua
+++ Doors/Script.lua
@@ -4319,6 +4319,6 @@ local CreditsTab = Window:MakeTab({
     PremiumOnly = false
 })
 
-CreditsTab:AddParagraph("Credits to","SindubsMini, plamen6789 to custom entites model!!. , Dreadmanita to every door script, (unknown) or spongus#7609 to crucifix script, screech#6908 the best script for the game DOORS!  ","My GitHub: SindubsMini - my discord: netzklap#7566","script is still in alpha version...")
+CreditsTab:AddParagraph("Credits to","SindubsMini, plamen6789 to custom entites model!!. , Dreadmania to every door script,  screech#6908 to crucifix script, screech#6908 the best script for the game DOORS!  ","My GitHub: SindubsMini - my discord: netzklap#7566","script is still in alpha version...")
 
 OrionLib:Init()
